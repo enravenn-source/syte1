7eaad03979c93431721b5a901309a72b384ac927
enravenn-source <enravenn@gmail.com>
2026-03-21 22:41:48 +0500

    app.py

--- app.py
+++ app.py
@@ -214,4 +214,4 @@ def send_contact():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, threaded=True)True)
+    app.run(host='0.0.0.0', port=port, threaded=True)
